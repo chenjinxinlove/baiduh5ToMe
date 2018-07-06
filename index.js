6e3b2816bcbb6ee7dc53ea5ebaac758aa04bc267
陈金鑫 <505150704@qq.com>
2018-07-06 09:44:00 +0800

    图片上传完成了一半

---
+++ index.js
@@ -0,0 +1,67 @@
+const fs  = require('fs');
+const http = require('http');
+const download = require('download');
+const needle = require('needle');
+const config = require('./src/config.js');
+
+const {url, port, paths} = config;
+
+const BAIDU_IMG = /https\:\/\/imgsa.+jpg/g;
+const BAIDU_CSS = /http\:\/\/.+css/g;
+const BAIDU_JS =  /https\:\/\/.+js/g;
+
+
+
+// (function() {
+//     const allArr = readHtmlMergeUrl('./src/yss.html');
+//     Promise.all(allArr.forEach(arr => {
+//         downFile(arr);
+//     })).then(() => {
+//         console.log('files downloaded!')
+//     })
+// })()
+
+// 读取的在html中的文件url
+
+function readHtmlMergeUrl (path) {
+    const str = fs.readFileSync(path, 'utf8');
+    const imgArr = str.match(BAIDU_IMG);
+    const cssArr = str.match(BAIDU_CSS);
+    const jsArr = str.match(BAIDU_JS);
+    return [imgArr, cssArr, jsArr];
+}
+
+// 下载文件
+
+function downFile (fileArr) {
+    return new Promise((resolve, reject) => {
+        Promise.all(fileArr.map(x => download(x, 'dist'))).then(() => {
+            resolve('com')
+        }).catch(err => reject(err))
+    })    
+}
+
+// 上传到自己的cdn  有问题
+function upload(filePath) {
+    var data = {
+        file: __dirname + filePath,
+        content_type: 'image/jpg'
+      };
+      
+      // the callback is optional, and needle returns a `readableStream` object
+      // that triggers a 'done' event when the request/response process is complete.
+      console.log(data)
+      needle
+        .post('http://fe.neibu.koolearn.com:3000/tool/uploader', data, {timeout: 5000, multipart: true })
+        .on('readable', function() { console.log('ddd') })
+        .on('done', function(err, resp) {
+          console.log('Ready-o!', resp, err);
+        })
+
+
+}
+
+upload('/dist/0824ab18972bd4076737650077899e510eb3095e.jpg')
+
+// 替换页面中的
+
---
+++ src/dz.js
@@ -0,0 +1,73 @@
+const arr = [{y: "https://imgsa.baidu.com/fex/pic/item/0b55b319ebc4b74526120eebc3fc1e178b82154c.jpg", x: "https://zky.koocdn.com/club/picture/c0acd5e63a5b44cb98173977b0969ffd.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/1ad5ad6eddc451dabe46b416bafd5266d116329e.jpg", x: "https://zky.koocdn.com/club/picture/7d69adc999c14099abadd05cf892e848.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/1b4c510fd9f9d72ac7c5b9e0d82a2834359bbbae.jpg", x: "https://zky.koocdn.com/club/picture/5f1e15b90a1143f7b0901754b05328cb.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/1c950a7b02087bf463b4cc14fed3572c10dfcff7.jpg", x: "https://zky.koocdn.com/club/picture/2e10a172785d408296ec51f5c10122cb.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/2e2eb9389b504fc2d1b3f0f8e9dde71191ef6d5a.jpg", x: "https://zky.koocdn.com/club/picture/79c4e6d8d823457499d18a4011009228.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/2e2eb9389b504fc23fef0af8e9dde71191ef6d9f.jpg", x: "https://zky.koocdn.com/club/picture/71ba91a44e0e49c58ae0526d9c14eeb2.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/3b292df5e0fe9925a4e029ad38a85edf8cb1715a.jpg", x: "https://zky.koocdn.com/club/picture/28436a62a409481087640be40ef32135.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/4a36acaf2edda3cc957c0edd0de93901203f92b7.jpg", x: "https://zky.koocdn.com/club/picture/8d07043d9b3c4913b9c9ab514631def6.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/4d086e061d950a7b851bf53906d162d9f2d3c925.jpg", x: "https://zky.koocdn.com/club/picture/2968d02d69854a0f868278fa600c480d.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/4e4a20a4462309f73de81bef7e0e0cf3d6cad64c.jpg", x: "https://zky.koocdn.com/club/picture/afe3ee202a06476a9b6c266efbab6a1b.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/5ab5c9ea15ce36d30dcac3cd36f33a87e850b105.jpg", x: "https://zky.koocdn.com/club/picture/ab49c3747f824197a6483efdca4712a1.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/6f061d950a7b0208e82a891c6ed9f2d3562cc868.jpg", x: "https://zky.koocdn.com/club/picture/cb8041a089be4fc48bde023f5f22d1bc.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/6f061d950a7b0208ff57981c6ed9f2d3562cc8b5.jpg", x: "https://zky.koocdn.com/club/picture/c06bb4c598ff4136ba12040a5355135a.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/8ad4b31c8701a18b026ca4b2922f07082938fe4c.jpg", x: "https://zky.koocdn.com/club/picture/6a7ae8d8f9f743c196c84711835ac010.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/35a85edf8db1cb1357eb7ceed154564e93584b48.jpg", x: "https://zky.koocdn.com/club/picture/740e8b93fb4840988f15063f18117234.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/43a7d933c895d143f1b252de7ff082025baf0702.jpg", x: "https://zky.koocdn.com/club/picture/89b51b46c2834bfe910b5247dfd0785a.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/50da81cb39dbb6fd143f59db0524ab18962b37e2.jpg", x: "https://zky.koocdn.com/club/picture/8113e81373394529a8a1a1629cd27c5f.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/55e736d12f2eb9387266c10fd9628535e4dd6fc0.jpg", x: "https://zky.koocdn.com/club/picture/988c72dbad7943d5881a142b2e358e57.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/63d9f2d3572c11df25e91ade6f2762d0f703c217.jpg", x: "https://zky.koocdn.com/club/picture/164e8302cf0b418492f21ecdd2098bae.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/71cf3bc79f3df8dce2a6396cc111728b461028b7.jpg", x: "https://zky.koocdn.com/club/picture/f99c5f94d60b4fbab50334cdce70e2b0.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/72f082025aafa40f100c7120a764034f79f01900.jpg", x: "https://zky.koocdn.com/club/picture/4ff5931bfd154627ae3f3184d9f78cdd.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/810a19d8bc3eb13525080e46aa1ea8d3fc1f447b.jpg", x: "https://zky.koocdn.com/club/picture/c9b92f416c3e4bf48549a9ef038816f8.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/0824ab18972bd4076737650077899e510eb3095e.jpg", x: "https://zky.koocdn.com/club/picture/f88198ef842e40f09665fa00891c2f05.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/838ba61ea8d3fd1f658f14c73c4e251f94ca5f4c.jpg", x: "https://zky.koocdn.com/club/picture/1d9eae97acb34757bf7332ac09cf7049.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/962bd40735fae6cd35d0179c03b30f2443a70fe2.jpg", x: "https://zky.koocdn.com/club/picture/dd90f01fe070448d812d56d6c8ef4fe0.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/962bd40735fae6cd3104139c03b30f2443a70f9e.jpg", x: "https://zky.koocdn.com/club/picture/ddf8bc91de434798a159beb62ad3b970.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/3812b31bb051f819fae1b5b7d6b44aed2f73e703.jpg", x: "https://zky.koocdn.com/club/picture/4c30462d6d79491ea0ebe8cb0ce8fe76.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/5366d0160924ab18d0e558ca39fae6cd7a890b5f.jpg", x: "https://zky.koocdn.com/club/picture/7a1585d9e67f4ac48cba3460173e0538.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/8326cffc1e178a82caf8c95dfa03738da877e803.jpg", x: "https://zky.koocdn.com/club/picture/682f8d3130c449e2b77e16e7d970020e.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/8326cffc1e178a82f147e45dfa03738da877e8d0.jpg", x: "https://zky.koocdn.com/club/picture/40a575cab24f4cbdacb8fa7c0d851241.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/29381f30e924b899d285d9c562061d950b7bf696.jpg", x: "https://zky.koocdn.com/club/picture/1cf2b6997cd3426fbb9596830759d9a3.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/29381f30e924b89924bbcfc562061d950b7bf64c.jpg", x: "https://zky.koocdn.com/club/picture/f39ab46693cd4588969aa81311218b83.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/83025aafa40f4bfba0e320a90f4f78f0f736181f.jpg", x: "https://zky.koocdn.com/club/picture/6e68eec97cbb45d8930470eebbf0b8af.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/622762d0f703918f3f158b375d3d269758eec403.jpg", x: "https://zky.koocdn.com/club/picture/9a72433267fd4bb59dd56f99126ee1b6.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/4034970a304e251f83af4405ab86c9177e3e53f6.jpg", x: "https://zky.koocdn.com/club/picture/4dbd8d1041cd44689da930e31c4fb67d.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/48540923dd54564eb0e7a4c4bfde9c82d0584f48.jpg", x: "https://zky.koocdn.com/club/picture/20047aac4e9b4296adc67a4e1e7f67db.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/a6efce1b9d16fdfa54817b13b88f8c5495ee7b5a.jpg", x: "https://zky.koocdn.com/club/picture/d0c37f953dab41439cddf07b4609144a.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/a8014c086e061d959ba581c577f40ad163d9ca68.jpg", x: "https://zky.koocdn.com/club/picture/1f5b3c03357b490892f8f448aeea8e74.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/ac4bd11373f0820202a12fc247fbfbedab641b1f.jpg", x: "https://zky.koocdn.com/club/picture/68f48d2ee33b469a89266e349a437288.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/ac345982b2b7d0a2d7efb860c7ef76094b369a2d.jpg", x: "https://zky.koocdn.com/club/picture/8e8e87e122974d6e8bbb9327f303df0b.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/ac345982b2b7d0a2ee51b360c7ef76094a369ab7.jpg", x: "https://zky.koocdn.com/club/picture/11716f957e4f4d599007c19833c42f88.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/b21bb051f8198618488d587946ed2e738ad4e601.jpg", x: "https://zky.koocdn.com/club/picture/732304b2611d4d5ba3be63a4bcf1ac94.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/b21c8701a18b87d6a1c51de20b0828381e30fd4c.jpg", x: "https://zky.koocdn.com/club/picture/5f4be824808847ecb64ac75522124eee.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/b90e7bec54e736d1c7e434f597504fc2d462695c.jpg", x: "https://zky.koocdn.com/club/picture/626d90a71d83447aa656c60d646fd286.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/b8014a90f603738dd87cb0dfbf1bb051f819ec1d.jpg", x: "https://zky.koocdn.com/club/picture/1afbb7b7a66f4867b831d24d66600544.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/bd3eb13533fa828b87b82b1ef11f4134960a5ab7.jpg", x: "https://zky.koocdn.com/club/picture/f4b39d722c9f4a5c8ebc0b2e11cfda18.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/c2fdfc039245d688e06e0959a8c27d1ed31b24da.jpg", x: "https://zky.koocdn.com/club/picture/e145fe28a08648c683ad0a2ced84b71f.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/ca1349540923dd5477f1e795dd09b3de9d824848.jpg", x: "https://zky.koocdn.com/club/picture/0c4ff704acba4377a56d333af45df5a7.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/caef76094b36acafc978200170d98d1000e99cb7.jpg", x: "https://zky.koocdn.com/club/picture/97c5f146b60f4de2bf3d7902bbc5c449.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/d0c8a786c9177f3e422042f07ccf3bc79f3d5632.jpg", x: "https://zky.koocdn.com/club/picture/b6c9860d15384252b6604400f4ae9e27.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/d439b6003af33a8735a340e7ca5c10385243b505.jpg", x: "https://zky.koocdn.com/club/picture/44e234a9370b4130a055fae81780015e.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/d1160924ab18972b86e7b737eacd7b899f510a69.jpg", x: "https://zky.koocdn.com/club/picture/e9a641f7129b4a6faae563f006f7da84.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/dcc451da81cb39dbf433deabdc160924aa18305f.jpg", x: "https://zky.koocdn.com/club/picture/1ce8f0ab808941329602f988b85e74f7.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/e7cd7b899e510fb35e7dcb6ad533c895d0430ce2.jpg", x: "https://zky.koocdn.com/club/picture/38356a2047bf47b987c2af1d941f58f6.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/eaf81a4c510fd9f96746f294292dd42a2934a4fe.jpg", x: "https://zky.koocdn.com/club/picture/d6cf04f3897d412b958cead99bf60567.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/faf2b2119313b07e1d25e6f500d7912396dd8cb7.jpg", x: "https://zky.koocdn.com/club/picture/e853854cbf8b43baa846548a558e5b12.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/fd039245d688d43fc727270f711ed21b0ff43b44.jpg", x: "https://zky.koocdn.com/club/picture/3f9bcf96873b46798417319184c97410.jpg"},
+{y: "https://imgsa.baidu.com/fex/pic/item/0b55b319ebc4b74526120eebc3fc1e178b82154c.jpg", x: "https://zky.koocdn.com/club/picture/c0acd5e63a5b44cb98173977b0969ffd.jpg"}]
+
+// 生成新的文件
+
+const fs  = require('fs');
+// let str = fs.readFileSync('./yss.html', 'utf8');
+
+//   arr.forEach(item => {
+//     str = str.replace(item.y, item.x)
+// })
+
+// fs.writeFileSync('./yssnew.html', str, 'utf8')
+
+
+const arrx = arr.map(item => item.x)
+fs.writeFileSync('./arrx.js', JSON.stringify(arrx), 'utf8')
